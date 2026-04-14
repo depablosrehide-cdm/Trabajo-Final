25eae78a4df8d6fd4e174e8a7a916d75709c6cbd
depablosrehide-cdm <depablosrehide@gmail.com>
2026-04-14 02:37:44 -0400

    nuevo arreglo de librería

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly as px
+import plotly.express as px 
 import io
 
 st.set_page_config(page_title="F1 Pit Stop Analysis", page_icon="🏎️", layout="wide")
